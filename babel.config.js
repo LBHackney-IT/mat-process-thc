8aab17807e84bb9491b95f6048dc8e27d1f71668
Chris <cjwaszczuk0@Live.com>
2019-12-03 15:28:17 +0000

    Import and set up lbh-frontend-react
    We add `babel-plugin-module-resolver` and TypeScript config to alias
    the deep paths of `lbh-frontend-react` until that library exports in
    a clean way.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  presets: [["next/babel", { "styled-jsx": { "babel-test": true } }]]
+  presets: [["next/babel", { "styled-jsx": { "babel-test": true } }]],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "lbh-frontend-react": ["./node_modules/lbh-frontend-react/dist/cjs"]
+        }
+      }
+    ]
+  ]
 };
