35903b8982b17c715f89f08be606395e2f7efb03
Chris <cjwaszczuk0@Live.com>
2019-12-03 15:28:17 +0000

    Import and set up lbh-frontend-react
    We add `babel-plugin-module-resolver` and TypeScript config to alias
    the deep paths of `lbh-frontend-react` until that library exports in
    a clean way.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
+import { ComponentRegister } from "lbh-frontend-react/helpers/ComponentRegister";
 import App from "next/app";
+import Link from "next/link";
 
 import "normalize.css";
+
+ComponentRegister.init({
+  components: {
+    Link: props => <Link {...props} />
+  }
+});
 
 export default App;
