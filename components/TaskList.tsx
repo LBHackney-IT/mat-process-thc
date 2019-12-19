d88a2916c34530122ea0d1f96735691a30dbdf95
F <f@dxw.com>
2019-12-19 12:01:58 +0000

    Create a `TaskList` component
    We want to be able to render a list of sections that need to be
    completed.

---
+++ components/TaskList.tsx
@@ -0,0 +1,76 @@
+import { Link } from "lbh-frontend-react/components/Link";
+import { List } from "lbh-frontend-react/components/List";
+import { Tag } from "lbh-frontend-react/components/Tag";
+import { nullAsUndefined } from "null-as-undefined";
+import PropTypes from "prop-types";
+import React from "react";
+
+/**
+ * An item in {@link TaskList}.
+ */
+export interface TaskListItem {
+  /**
+   * The name of the task.
+   */
+  name: string;
+
+  /**
+   * A function to be passed to the button for the task list.
+   */
+  href: string;
+
+  /**
+   * The status to display against the task.
+   *
+   * If undefined, the status is replaced by a link to the task. To not show
+   * anything, pass an empty string.
+   */
+  status?: string | null;
+}
+
+export interface TaskListprops {
+  items: TaskListItem[];
+}
+
+export const TaskList = (props: TaskListprops): JSX.Element => {
+  const { items } = props;
+  return (
+    <>
+      <List
+        className="task-list"
+        items={items.map(({ name, href, status }) => (
+          <>
+            <span>{name}</span>
+            {status !== "" && (
+              <span>
+                {nullAsUndefined(status) === undefined ? (
+                  <Link href={href}>Start</Link>
+                ) : (
+                  <Tag>Completed</Tag>
+                )}
+              </span>
+            )}
+          </>
+        ))}
+      />
+      <style jsx>{`
+        :global(.task-list li) {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-bottom: 1em;
+        }
+      `}</style>
+    </>
+  );
+};
+
+TaskList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+      status: PropTypes.string
+    }).isRequired
+  ).isRequired
+};
