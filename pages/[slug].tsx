3344b9424f0975f67c8b4ee1b73aaa97d2fa023f
F <f@dxw.com>
2020-01-02 16:30:48 +0000

    Switch to using a function component for the process page
    We want to be able to assert that it's a `NextPage`, and infer the types
    of its static functions.

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,5 +1,5 @@
-import { NextPageContext } from "next";
-import React, { Component } from "react";
+import { NextPage, NextPageContext } from "next";
+import React from "react";
 import { Orchestrator } from "remultiform/orchestrator";
 
 import { TenancySummary } from "../components/TenancySummary";
@@ -8,72 +8,69 @@ import steps from "../steps";
 import processRef from "../storage/processRef";
 import Storage from "../storage/Storage";
 
-interface ProcessPageProps {
+interface Props {
   slug: string;
 }
 
-class ProcessPage extends Component<ProcessPageProps> {
-  static getInitialProps({ query }: NextPageContext): ProcessPageProps {
-    const { slug } = query;
+const ProcessPage: NextPage<Props> = ({ slug }: Props) => {
+  const currentStep = steps.find(step => step.step.slug === slug);
 
-    return { slug: slug as string };
+  if (!currentStep) {
+    console.error(`No step matches slug ${slug}`);
+
+    return <div>Something went wrong!</div>;
   }
 
-  render(): React.ReactElement {
-    const { slug } = this.props;
+  const content = (
+    <>
+      <TenancySummary
+        address="1 Mare Street, London, E8 3AA"
+        tenants={["Jane Doe", "John Doe"]}
+        tenureType="Introductory"
+        startDate="1 January 2019"
+      />
+      <Orchestrator
+        context={Storage.Context}
+        initialSlug={slug}
+        steps={steps.map(step => step.step)}
+        manageStepTransitions={false}
+        provideDatabase={false}
+        onNextStep={async (): Promise<void> => {
+          try {
+            await Storage.updateLastModified(processRef);
+          } catch (error) {
+            // This is invisible to the user, so we should do something to
+            // display it to them.
+            console.error(error);
+          }
+        }}
+      />
+    </>
+  );
+  let page: React.ReactElement;
 
-    const currentStep = steps.find(step => step.step.slug === slug);
-
-    if (!currentStep) {
-      console.error(`No step matches slug ${slug}`);
-
-      return <div>Something went wrong!</div>;
-    }
-
-    const content = (
-      <>
-        <TenancySummary
-          address="1 Mare Street, London, E8 3AA"
-          tenants={["Jane Doe", "John Doe"]}
-          tenureType="Introductory"
-          startDate="1 January 2019"
-        />
-        <Orchestrator
-          context={Storage.Context}
-          initialSlug={slug}
-          steps={steps.map(step => step.step)}
-          manageStepTransitions={false}
-          provideDatabase={false}
-          onNextStep={async (): Promise<void> => {
-            try {
-              await Storage.updateLastModified(processRef);
-            } catch (error) {
-              // This is invisible to the user, so we should do something to
-              // display it to them.
-              console.error(error);
-            }
-          }}
-        />
-      </>
+  if (currentStep.heading) {
+    page = (
+      <MainLayout title={currentStep.title} heading={currentStep.heading}>
+        {content}
+      </MainLayout>
     );
-    let page: React.ReactElement;
+  } else if (currentStep.title) {
+    page = <MainLayout title={currentStep.title}>{content}</MainLayout>;
+  } else {
+    console.error("At least one of title or heading is required");
 
-    if (currentStep.heading) {
-      page = (
-        <MainLayout title={currentStep.title} heading={currentStep.heading}>
-          {content}
-        </MainLayout>
-      );
-    } else if (currentStep.title) {
-      page = <MainLayout title={currentStep.title}>{content}</MainLayout>;
-    } else {
-      console.error("At least one of title or heading is required");
-
-      page = <MainLayout title={slug}>{content}</MainLayout>;
-    }
-
-    return page;
+    page = <MainLayout title={slug}>{content}</MainLayout>;
   }
-}
+
+  return page;
+};
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+ProcessPage.getInitialProps = ({ query }: NextPageContext): Props => {
+  const { slug } = query;
+
+  return { slug: slug as string };
+};
 
 export default ProcessPage;
