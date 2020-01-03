be27af801394b98a7867e7058376e2461a8fe346
F <f@dxw.com>
2020-01-03 18:57:16 +0000

    Create a `TextInput`

---
+++ components/TextInput.tsx
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import React from "react";
+import {
+  DynamicComponentControlledProps,
+  DynamicComponent
+} from "remultiform/component-wrapper";
+
+type Props = DynamicComponentControlledProps<string> & {
+  label: string;
+  name: string;
+};
+
+export const TextInput = (props: Props): JSX.Element => {
+  const { label, name, value, onValueChange } = props;
+
+  const labelId = `${name}-label`;
+  const inputId = `${name}-input`;
+
+  return (
+    <div>
+      <label id={labelId} htmlFor={inputId}>
+        {label}
+      </label>
+      <input
+        id={inputId}
+        name={name}
+        type="text"
+        value={value}
+        onChange={(event): void => {
+          onValueChange(event.target.value);
+        }}
+        aria-labelledby={labelId}
+      />
+
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-direction: column;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  ...DynamicComponent.controlledPropTypes(PropTypes.string.isRequired),
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+};
