8eeab910da36166d467ca6047aa1a8719539079e
F <f@dxw.com>
2020-01-03 14:32:28 +0000

    Increase the feature test timeout

--- __tests__/jest.setup.feature.ts
+++ __tests__/jest.setup.feature.ts
@@ -4,7 +4,7 @@ import yn from "yn";
 
 import WebDriverWrapper from "./helpers/webdriver/WebDriverWrapper";
 
-jest.setTimeout(10 * 1000);
+jest.setTimeout(20 * 1000);
 
 if (yn(process.env.CI)) {
   jest.retryTimes(3);
