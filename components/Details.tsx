c11569ba3b35ae8f6390419a02573025cdc7cf42
F <f@dxw.com>
2020-01-03 16:35:34 +0000

    Create a `Details` component

---
+++ components/Details.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+interface Props {
+  summary: string;
+  children: React.ReactNode;
+}
+
+export const Details = (props: Props): JSX.Element => {
+  const { summary, children } = props;
+
+  return (
+    <>
+      <details>
+        <summary>{summary}</summary>
+        {children}
+      </details>
+
+      <style jsx>{`
+        details {
+          margin-top: 20px;
+        }
+
+        summary {
+          text-decoration: underline;
+          font-family: "Montserrat";
+        }
+
+        summary:hover {
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  );
+};
+
+Details.propTypes = {
+  summary: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+};
