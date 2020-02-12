e4255599892ca046b2d51dbb13917944415a8c95
Emma Lewis <emmarachellewis@gmail.com>
2020-02-12 17:13:24 +0000

    Use inputs from lbh-frontend-react for TextInput

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -3,45 +3,33 @@ import {
   DynamicComponentControlledProps,
   DynamicComponent
 } from "remultiform/component-wrapper";
+import { Input, InputType } from "lbh-frontend-react/components/Input";
 
 import PropTypes from "../helpers/PropTypes";
 
-type Props = DynamicComponentControlledProps<string> & {
+type TextInputProps = DynamicComponentControlledProps<string> & {
   label: string;
   name: string;
 };
 
-export const TextInput: React.FunctionComponent<Props> = props => {
+export const TextInput: React.FunctionComponent<TextInputProps> = props => {
   const { label, name, value, onValueChange, required, disabled } = props;
 
   const labelId = `${name}-label`;
   const inputId = `${name}-input`;
 
   return (
-    <div>
-      <label id={labelId} htmlFor={inputId}>
-        {label}
-      </label>
-      <input
-        id={inputId}
-        name={name}
-        type="text"
-        value={value}
-        required={required}
-        disabled={disabled}
-        onChange={(event): void => {
-          onValueChange(event.target.value);
-        }}
-        aria-labelledby={labelId}
-      />
-
-      <style jsx>{`
-        div {
-          display: flex;
-          flex-direction: column;
-        }
-      `}</style>
-    </div>
+    <Input
+      id={inputId}
+      name={name}
+      label={{ id: labelId, children: label }}
+      type={InputType.Text}
+      value={value}
+      required={required}
+      disabled={disabled}
+      onChange={onValueChange}
+      aria-labelledby={labelId}
+    />
   );
 };
 
