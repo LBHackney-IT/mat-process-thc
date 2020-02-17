3a0c6a74ab5efeac261bf6b58a959cd6968cc52e
F <f@dxw.com>
2020-02-17 12:12:37 +0000

    Drop the unused offline tests
    They don't work and there are no concrete plans on getting them working.

--- __tests__/features/prefetch-process-data.feature
+++ __tests__/features/prefetch-process-data.feature
@@ -10,17 +10,3 @@ Feature: Prefetch process data
     And I wait for the data to be fetched
     Then I should see the tenancy details
     And I should be able to continue
-
-# Scenario: Starting the process while offline
-#   Given I am offline
-#   When I start the process
-#   Then I should see that I need to go online to continue
-#   And I shouldn't be able to continue
-
-# Scenario: Going online while starting the process
-#   Given I am offline
-#   When I start the process
-#   And I go online
-#   And I wait for the data to be fetched
-#   Then the data for the process should be available
-#   And I should be able to continue
--- __tests__/features/prefetch-process-data.steps.ts
+++ __tests__/features/prefetch-process-data.steps.ts
@@ -1,9 +1,8 @@
-import { DefineStepFunction, defineFeature, loadFeature } from "jest-cucumber";
-
+import { defineFeature, DefineStepFunction, loadFeature } from "jest-cucumber";
+import Expect from "../helpers/Expect";
 import Given from "../helpers/steps/Given";
 import Then from "../helpers/steps/Then";
 import When from "../helpers/steps/When";
-import Expect from "../helpers/Expect";
 
 const thenIShouldSeeTheTenancyDetails = (
   defineStep: DefineStepFunction
@@ -25,27 +24,4 @@ defineFeature(loadFeature("./prefetch-process-data.feature"), test => {
     thenIShouldSeeTheTenancyDetails(defineStep);
     Then.iShouldBeAbleToContinue(defineStep);
   });
-
-  // test("Starting the process while offline", ({ defineStep, then }) => {
-  //   Given.iAmOffline(defineStep);
-
-  //   When.iStartTheProcess(defineStep);
-
-  //   then("I should see that I need to go online to continue", async () => {
-  //     await Expect.pageToContain("Please go online to continue");
-  //   });
-
-  //   Then.iShouldntBeAbleToContinue(defineStep);
-  // });
-
-  // test("Going online while starting the process", ({ defineStep }) => {
-  //   Given.iAmOffline(defineStep);
-
-  //   When.iStartTheProcess(defineStep);
-  //   When.iGoOnline(defineStep);
-  //   When.iWaitForTheDataToBeFetched(defineStep);
-
-  //   thenIShouldSeeTheTenancyDetails(defineStep);
-  //   Then.iShouldBeAbleToContinue(defineStep);
-  // });
 });
