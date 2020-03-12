984f4e3efa91d266b4b5fc23da5b381b03fa19ef
F <f@dxw.com>
2020-03-12 18:03:25 +0000

    Create /pause and /paused based on /submit and /confirmed
    They are very similar, but may diverge in the future, so it doesn't make
    sense to abstract them at this point.

---
+++ pages/thc/[processRef]/pause.tsx
@@ -0,0 +1,156 @@
+import {
+  Button,
+  ErrorMessage,
+  PageAnnouncement,
+  Paragraph
+} from "lbh-frontend-react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import ProgressBar from "../../../components/ProgressBar";
+import persistProcessData from "../../../helpers/persistProcessData";
+import urlsForRouter from "../../../helpers/urlsForRouter";
+import useOnlineWithRetry from "../../../helpers/useOnlineWithRetry";
+import MainLayout from "../../../layouts/MainLayout";
+import PageSlugs, { urlObjectForSlug } from "../../../steps/PageSlugs";
+import PageTitles from "../../../steps/PageTitles";
+
+const PausePage: NextPage = () => {
+  const router = useRouter();
+  const online = useOnlineWithRetry();
+  const [progress, setProgress] = useState(0);
+  const [saving, setSaving] = useState(false);
+  const [saveError, setSaveError] = useState();
+
+  // TODO: Replace these with the real values!
+  const address = "1 Mare Street, London, E8 3AA";
+  const tenants = ["Jane Doe", "John Doe"];
+
+  let content: React.ReactElement;
+
+  if (online.loading) {
+    content = (
+      <Paragraph>
+        We are currently checking your online status. Please wait...
+      </Paragraph>
+    );
+  } else {
+    content = (
+      <PageAnnouncement title="Process pause pending">
+        <Paragraph>
+          {online.result
+            ? "You are online."
+            : "You are currently working offline."}
+        </Paragraph>
+        <Paragraph>
+          The Tenancy and Household Check for the tenancy at {address}, occupied
+          by {tenants.join(", ")} has been saved to your device but still needs
+          to be saved to your work tray so you can resume it later.
+        </Paragraph>
+        <Paragraph>
+          <strong>You need to be online on this device to continue.</strong>
+        </Paragraph>
+        <Paragraph>
+          If you can&apos;t go online now, when you are next online{" "}
+          <strong>on this device</strong>, please come back to this Tenancy and
+          Household Check from your work tray and click on the &lsquo;Save and
+          continue later&rsquo; button below that will become able to be
+          clicked.
+        </Paragraph>
+        {!online.error && online.result && (
+          <Paragraph>
+            <strong>You are online</strong>, and can save this Tenancy and
+            Household Check to your work tray now.
+          </Paragraph>
+        )}
+      </PageAnnouncement>
+    );
+  }
+
+  const { href, as } = urlsForRouter(
+    router,
+    urlObjectForSlug(router, PageSlugs.Paused)
+  );
+
+  const disabled =
+    online.loading ||
+    Boolean(online.error) ||
+    !online.result ||
+    saving ||
+    !href.pathname ||
+    !as.pathname;
+
+  return (
+    <MainLayout title={PageTitles.Submit}>
+      {online.error && (
+        <ErrorMessage>
+          Something went wrong while checking your online status. Please reload
+          the page and try again. If the problem persists, please try reopening
+          this process from your worktray.
+        </ErrorMessage>
+      )}
+
+      {saveError && (
+        <ErrorMessage>
+          Something went wrong. Please try going back and pausing this process
+          again.
+        </ErrorMessage>
+      )}
+
+      {content}
+
+      {saving && (
+        <ProgressBar
+          progress={progress}
+          incompleteLabel={saveError ? "Error" : "Saving..."}
+          completeLabel={saveError ? "Error" : "Saved"}
+        />
+      )}
+
+      {!saving && (
+        <>
+          <Button
+            disabled={disabled}
+            preventDoubleClick
+            onClick={async (): Promise<void> => {
+              if (!href.pathname || !as.pathname) {
+                return;
+              }
+
+              try {
+                setSaving(true);
+
+                await persistProcessData(router, setProgress);
+                await router.push(href, as);
+              } catch (err) {
+                console.error(err);
+                setSaveError(err);
+              }
+            }}
+            data-testid="submit"
+          >
+            {disabled
+              ? "Waiting for connectivity..."
+              : "Save and continue later"}
+          </Button>
+          <Button
+            className="lbh-button--secondary govuk-button--secondary"
+            onClick={(): void => {
+              router.back();
+            }}
+          >
+            Cancel and continue now
+          </Button>
+        </>
+      )}
+
+      <style jsx>{`
+        :global(button:not(:last-child)) {
+          margin-right: 1em;
+        }
+      `}</style>
+    </MainLayout>
+  );
+};
+
+export default PausePage;
---
+++ pages/thc/[processRef]/paused.tsx
@@ -0,0 +1,31 @@
+import { Button } from "lbh-frontend-react/components/Button";
+import { PageAnnouncement } from "lbh-frontend-react/components/PageAnnouncement";
+import { Paragraph } from "lbh-frontend-react/components/typography/Paragraph";
+import { NextPage } from "next";
+import React from "react";
+import MainLayout from "../../../layouts/MainLayout";
+import PageTitles from "../../../steps/PageTitles";
+
+const PausedPage: NextPage = () => {
+  // TODO: Replace these with the real values!
+  const address = "1 Mare Street, London, E8 3AA";
+  const tenants = ["Jane Doe", "John Doe"];
+
+  return (
+    <MainLayout title={PageTitles.Confirmed}>
+      <PageAnnouncement title="Process paused">
+        <Paragraph>
+          The Tenancy and Household Check for the tenancy at {address}, occupied
+          by {tenants.join(", ")} has been paused and saved to your work tray
+          ready to continue later.
+        </Paragraph>
+      </PageAnnouncement>
+
+      <Paragraph>
+        <Button>Return to my work tray</Button>
+      </Paragraph>
+    </MainLayout>
+  );
+};
+
+export default PausedPage;
