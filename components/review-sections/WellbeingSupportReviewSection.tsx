1570735a3c6755554e44d3f26e88e218f05b98d9
F <f@dxw.com>
2020-04-21 17:13:41 +0100

    Fix the casing on an import

--- components/review-sections/WellbeingSupportReviewSection.tsx
+++ components/review-sections/WellbeingSupportReviewSection.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import useReviewSectionRows from "../../helpers/useReviewSectionRows";
-import WellbeingSupportSteps from "../../steps/wellbeing-support";
+import wellbeingSupportSteps from "../../steps/wellbeing-support";
 import Storage from "../../storage/Storage";
 import { ReviewSection } from "../ReviewSection";
 
 export const WellbeingSupportReviewSection: React.FunctionComponent = () => {
   const rows = useReviewSectionRows(
     Storage.ProcessContext,
-    WellbeingSupportSteps
+    wellbeingSupportSteps
   );
 
   return (
