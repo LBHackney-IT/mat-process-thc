467fafec7448b421b944247efb4b0f026acf41ba
F <f@dxw.com>
2020-04-22 17:13:04 +0100

    Add second visit to the failed attempts announcement

--- components/PreviousAttemptsAnnouncement.tsx
+++ components/PreviousAttemptsAnnouncement.tsx
@@ -1,5 +1,9 @@
 import formatDate from "date-fns/format";
-import { PageAnnouncement, SummaryList } from "lbh-frontend-react/components";
+import {
+  PageAnnouncement,
+  Paragraph,
+  SummaryList,
+} from "lbh-frontend-react/components";
 import { useRouter } from "next/router";
 import React from "react";
 import failedAttemptReasonCheckboxes from "../helpers/failedAttemptReasonCheckboxes";
@@ -7,51 +11,91 @@ import getProcessRef from "../helpers/getProcessRef";
 import useDataValue from "../helpers/useDataValue";
 import Storage from "../storage/Storage";
 
-const PreviousAttemptsAnnouncement: React.FunctionComponent = () => {
-  const router = useRouter();
-  const processRef = getProcessRef(router);
+const Value: React.FunctionComponent<{
+  date?: string;
+  reasons?: string[];
+  notes?: string;
+}> = (props) => {
+  const date = props.date && formatDate(new Date(props.date), "d MMMM yyyy");
+  const reasons = props.reasons || [];
+  const notes = props.notes;
 
-  const firstFailedAttempt = useDataValue(
-    Storage.ProcessContext,
-    "unableToEnter",
-    processRef,
-    (values) =>
-      processRef ? values[processRef]?.firstFailedAttempt : undefined
-  );
-
-  const firstDate =
-    firstFailedAttempt.result?.date &&
-    formatDate(new Date(firstFailedAttempt.result?.date), "d MMMM yyyy");
-  const firstValue = firstFailedAttempt.result?.value || [];
-  const firstNotes = firstFailedAttempt.result?.notes;
-
-  const firstReasons = failedAttemptReasonCheckboxes
-    .filter((checkbox) => firstValue.includes(checkbox.value))
+  const reason = failedAttemptReasonCheckboxes
+    .filter((checkbox) => reasons.includes(checkbox.value))
     .map((checkbox) => checkbox.label)
     .join(", ");
 
   return (
+    <Paragraph>
+      <span>{date}</span>
+      {reason && (
+        <span>
+          <br />
+          {reason}
+        </span>
+      )}
+      {notes && (
+        <span>
+          <br />
+          {notes}
+        </span>
+      )}
+    </Paragraph>
+  );
+};
+
+const PreviousAttemptsAnnouncement: React.FunctionComponent = () => {
+  const router = useRouter();
+  const processRef = getProcessRef(router);
+
+  const unableToEnter = useDataValue(
+    Storage.ProcessContext,
+    "unableToEnter",
+    processRef,
+    (values) => (processRef ? values[processRef] : undefined)
+  );
+
+  const madeFirstAttempt = Boolean(
+    unableToEnter.result?.firstFailedAttempt?.date
+  );
+
+  if (!madeFirstAttempt) {
+    return null;
+  }
+
+  const rows = [
+    {
+      key: "First attempt",
+      value: (
+        <Value
+          date={unableToEnter.result?.firstFailedAttempt?.date}
+          reasons={unableToEnter.result?.firstFailedAttempt?.value}
+          notes={unableToEnter.result?.firstFailedAttempt?.notes}
+        />
+      ),
+    },
+  ];
+
+  const madeSecondAttempt = Boolean(
+    unableToEnter.result?.secondFailedAttempt?.date
+  );
+
+  if (madeSecondAttempt) {
+    rows.push({
+      key: "Second attempt",
+      value: (
+        <Value
+          date={unableToEnter.result?.secondFailedAttempt?.date}
+          reasons={unableToEnter.result?.secondFailedAttempt?.value}
+          notes={unableToEnter.result?.secondFailedAttempt?.notes}
+        />
+      ),
+    });
+  }
+
+  return (
     <PageAnnouncement title="Previous attempts">
-      <SummaryList
-        rows={[
-          {
-            key: "First attempt",
-            value: firstFailedAttempt.loading ? (
-              "Loading..."
-            ) : (
-              <>
-                {firstDate}: {firstReasons}
-                {firstNotes === undefined || (
-                  <>
-                    <br />
-                    {firstNotes}
-                  </>
-                )}
-              </>
-            ),
-          },
-        ]}
-      />
+      {unableToEnter.loading ? "Loading..." : <SummaryList rows={rows} />}
     </PageAnnouncement>
   );
 };
