d547ddae08a7f5451d2a4371b000995bc2973649
F <f@dxw.com>
2020-04-23 18:58:50 +0100

    Add real tenancy details to the submit page

--- pages/thc/[processRef]/submit.tsx
+++ pages/thc/[processRef]/submit.tsx
@@ -8,24 +8,36 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import ProgressBar from "../../../components/ProgressBar";
+import getProcessRef from "../../../helpers/getProcessRef";
 import persistProcessData from "../../../helpers/persistProcessData";
 import { transferProcessToManager } from "../../../helpers/transferProcess";
 import urlsForRouter from "../../../helpers/urlsForRouter";
+import useDataValue from "../../../helpers/useDataValue";
 import useOnlineWithRetry from "../../../helpers/useOnlineWithRetry";
 import MainLayout from "../../../layouts/MainLayout";
 import PageSlugs, { urlObjectForSlug } from "../../../steps/PageSlugs";
 import PageTitles from "../../../steps/PageTitles";
+import Storage from "../../../storage/Storage";
 
 const SubmitPage: NextPage = () => {
   const router = useRouter();
+  const processRef = getProcessRef(router);
   const online = useOnlineWithRetry();
   const [progress, setProgress] = useState(0);
   const [submitting, setSubmitting] = useState(false);
   const [submitError, setSubmitError] = useState();
 
-  // TODO: Replace these with the real values!
-  const address = "1 Mare Street, London, E8 3AA";
-  const tenants = ["Jane Doe", "John Doe"];
+  const residentData = useDataValue(
+    Storage.ExternalContext,
+    "residents",
+    processRef,
+    (values) => (processRef ? values[processRef] : undefined)
+  );
+
+  const address = residentData.result?.address.join(", ");
+  const tenants = (residentData.result?.tenants || [])
+    .map((tenant) => tenant.fullName)
+    .join(", ");
 
   let content: React.ReactElement;
 
@@ -43,26 +55,32 @@ const SubmitPage: NextPage = () => {
             ? "You are online."
             : "You are currently working offline."}
         </Paragraph>
-        <Paragraph>
-          The Tenancy and Household Check for the tenancy at {address}, occupied
-          by {tenants.join(", ")} has been saved to your device ready to be sent
-          to your manager for review.
-        </Paragraph>
-        <Paragraph>
-          <strong>You need to be online on this device to continue.</strong>
-        </Paragraph>
-        <Paragraph>
-          If you can&apos;t go online now, when you are next online{" "}
-          <strong>on this device</strong>, please come back to this Tenancy and
-          Household Check from your work tray and click on the &lsquo;Save and
-          submit to manager&rsquo; button below that will become able to be
-          clicked.
-        </Paragraph>
-        {!online.error && online.result && (
-          <Paragraph>
-            <strong>You are online</strong>, and can submit this Tenancy and
-            Household Check to your manager now.
-          </Paragraph>
+        {residentData.loading ? (
+          "Loading..."
+        ) : (
+          <>
+            <Paragraph>
+              The Tenancy and Household Check for the tenancy at {address},
+              occupied by {tenants} has been saved to your device ready to be
+              sent to your manager for review.
+            </Paragraph>
+            <Paragraph>
+              <strong>You need to be online on this device to continue.</strong>
+            </Paragraph>
+            <Paragraph>
+              If you can&apos;t go online now, when you are next online{" "}
+              <strong>on this device</strong>, please come back to this Tenancy
+              and Household Check from your work tray and click on the
+              &lsquo;Save and submit to manager&rsquo; button below that will
+              become able to be clicked.
+            </Paragraph>
+            {!online.error && online.result && (
+              <Paragraph>
+                <strong>You are online</strong>, and can submit this Tenancy and
+                Household Check to your manager now.
+              </Paragraph>
+            )}
+          </>
         )}
       </PageAnnouncement>
     );
