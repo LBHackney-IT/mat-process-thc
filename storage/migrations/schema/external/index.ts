478683a0bb72aabd679007566b1a0fec2ad2c77b
George Eaton <george@dxw.com>
2020-04-23 12:17:02 +0100

    Add Officer to External Schema
    This will contain officer data from the MaT data blob - currently only the
    `fullName` of the officer who carried out the check

--- storage/migrations/schema/external/index.ts
+++ storage/migrations/schema/external/index.ts
@@ -1,9 +1,11 @@
 import { Upgrade } from "remultiform/database";
 import ExternalDatabaseSchema from "../../../ExternalDatabaseSchema";
 import from0 from "./from0";
+import from1 from "./from1";
 
 export default {
   0: from0,
+  1: from1,
 } as {
   [n: number]:
     | ((upgrade: Upgrade<ExternalDatabaseSchema["schema"]>) => void)
